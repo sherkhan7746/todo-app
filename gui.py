1ad7cbd6f7a0f0e1cb7816ffb3f7624808a5aac3
sherkhan7746 <sherkhan1167746@gmail.com>
2023-12-29 23:24:01 +0500

    create basic gui without backend

--- gui.py
+++ gui.py
@@ -4,6 +4,6 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do")
 add_button = sg.Button("Add")
-window = sg.Window("My to-do App", [[label],[input_box, add_button]])
+window = sg.Window("My to-do App", [[label], [input_box, add_button]])
 window.read()
 window.close()
